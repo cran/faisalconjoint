51903cc9a953d71a2b419fab2f9726c60d334877
Faisal Afzal Siddiqui <brc.khi@gmail.com>
2012-07-04 00:00:00 +0000

    version 1.10

--- R/faisalconjoint.R
+++ R/faisalconjoint.R
@@ -1,14 +1,14 @@
 faisalconjoint <-
 function(con,lal)
 {
-mf  <<- con
-level1 <<- lal
+mf = con
+level1 = lal
 
-uth <<- names(mf)
-level2 <<- NA
+uth = names(mf)
+level2 = NA
 Level = rbind (level1, level2)
 
-## Convert data file into matrix
+## convert data file into matrix
 dm=data.matrix(mf)
 
 ## total variable and total data
@@ -16,24 +16,22 @@ tc=NCOL(mf)
 tr=NROW(mf)
 tc1=tc-1
 
-## Takeing mean of every factor, level wise
+## takeing mean of every factor, level wise
 mm=0
 k=0
 Factor=0
 
-for (i in 2:tc1)
-{
-for (j in 1:max( dm [,i]))
-{	
+for (i in 2:tc1){
+for (j in 1:max( dm [,i])){
 k=k+1
 mm [k] <- mean(dm[,tc] [dm [,i]== j ] )
-Factor [k] <- uth [i]		
-}		
-}					
-
+Factor [k] <- uth [i]
+}
+}
 Factor [k+1] <- "Constant"
+
 # calculation of Path Worth Utilities
-Utility=mm-mean(mm)			
+Utility=mm-mean(mm)
 Utility [k+1] <- mean(mm)
 hd1=data.frame(Factor,Level,Utility)
 
@@ -63,20 +61,40 @@ for (sr in 1:kk){}
 hd2=data.frame(Factor1,Percentage)
 
 # Calculation for bar plot in order
-bar <- barplot( Percentage [order(-Percentage)], main = "Conjoint Factor Importance",
+bar <- barplot( Percentage [order(-Percentage)],
+main = "Faisal Conjoint Model: Factor Importance",
 xlab="Factors", ylab="Percentage",
 names.arg = Factor1 [order(-Percentage)],
 border="blue",
 density= sort(Percentage,decreasing = TRUE),
 ylim = c(0, 100) )
 imp <- Percentage [order(-Percentage)]
+text(bar, imp + 3, format(imp), xpd = TRUE)
+
+jpeg('FCMgraph.jpg')
+bar <- barplot( Percentage [order(-Percentage)],
+main = "Faisal Conjoint Model: Factor Importance",
+xlab="Factors", ylab="Percentage",
+names.arg = Factor1 [order(-Percentage)],
+border="blue",
+density= sort(Percentage,decreasing = TRUE),
+ylim = c(0, 100) )
+imp <- Percentage [order(-Percentage)]
+text(bar, imp + 3, format(imp), xpd = TRUE)
+dev.off()
 
 # display result
+writeLines("** Faisal Conjoint Model: Part-worth utilities **")
+print(hd1)
+writeLines(" ")
+writeLines("** Faisal Conjoint Model: Factor Importance **")
+print(hd2)
+writeLines(" ")
+writeLines("Note: Graph FCMgraph.jpg is automatically save in current working directory.")
+
+# display result on screen only
 View (hd1, "Conjoint Parth Worth Utility")
 View (hd2, "Conjoint Factor Importance")
 
-# display Graph
-text(bar, imp + 3, format(imp), xpd = TRUE)
-
+# for result at R prompt write "faisalconjoint(candies_data,candies_levels)"
 }
-
